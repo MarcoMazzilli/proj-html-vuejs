9882e696030942387d5559d1a66d9c0886553a27
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-05-09 17:53:31 +0200

    [Add] Jumbotron section

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -9,8 +9,22 @@ export default {
   <div class="wrapper">
 
     <div class="container">
+      <div class="hero-description">
 
-      <h1>Jumbotron</h1>
+        <div class="title">
+          <h1> Welcome to  <span>DogMilo</span> Pets </h1>
+        </div>
+
+        <div class="description">
+          <span> Lorem Ipsum is simply dummy text of the printing and typesetting industry. </span>
+        </div>
+
+        <div class="cta-buttons">
+          <span>Get Started</span>
+          <a href="#"><img src="../../public/jumbotron-img/play.png" alt="Play png"></a>
+        </div>
+
+      </div>
       
       
     </div>
@@ -22,8 +36,75 @@ export default {
 @import "../scss/general/variables";
 
 .wrapper{
-  min-height: 575px;
-  background-color: rosybrown;
+  height: 575px;
+  background-image: url(../../public/jumbotron-img/b.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  position: relative;
+
+  .container{
+    height: 100%;
+    display: flex;
+    align-items: center;
+
+    .hero-description{
+    
+      width: 500px;
+      height: 300px;
+
+      .title{
+        width: 80%;
+        margin-bottom: 20px;
+        h1{
+          color: white;
+          font-size: 3rem;
+          font-weight: 900;
+          span{
+            font-weight: 400;
+          }
+        }
+      }
+      .description{
+        width: 85%;
+        margin-bottom: 35px;
+        span{
+          color: white;
+          font-size: 1.2rem;
+        }
+      }
+      .cta-buttons{
+        display: flex;
+        align-items: center;
+        gap: 10px;      
+
+        span:first-child{
+          font-size: 1.5rem;
+          font-weight: 600;
+          color: white;
+          background-color: $brandMain;
+          padding: 10px 33px;
+          border-radius: 50px;
+          cursor: pointer;
+          transition: .3s;
+
+          &:hover{
+            background-color: $hoverBtn;
+          }
+        }
+
+        a img{
+          height: 60px;
+          padding: 10px;
+          border-radius: 10px;
+          background-color: rgb(112, 208, 247);
+          cursor: pointer;
+
+          
+        }
+      }
+    }
+  }
 }
 
 
